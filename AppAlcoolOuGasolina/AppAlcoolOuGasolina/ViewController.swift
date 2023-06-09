6e6e47801815cb14769b0cfe0c1973642ee0d2ca
lclaudiomattoss <lclaudiomattoss@gmail.com>
2023-06-09 19:03:58 -0300

    Criação do projeto
    O projeto foi criado e configurado para ViewCode

---
+++ AppAlcoolOuGasolina/AppAlcoolOuGasolina/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppAlcoolOuGasolina
+//
+//  Created by Luiz Claudio Mattos da Silva on 09/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
